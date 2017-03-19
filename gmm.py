8ce297fb2c1c60c091e0a1f1663ca9e09c55efd3
Harikrishnan R <harikrishnanr195@gmail.com>
2017-03-19 17:12:13 +0530

    Imports image data, find_val func done

--- gmm.py
+++ gmm.py
@@ -1,31 +1,33 @@
 import numpy as np
 import cv2
 import math
-
+from numpy.linalg import inv
+from numpy.linalg import det
 
 def find_val(w,mu,sigma,x):
     delta=x-mu
     deltat=np.transpose(delta)
-    covariance=delta*deltat
-    cinv=np.linalg.inv(covariance)
-    m=w*math.exp(-0.5*delta*cinv*deltat)
-    d=math.pow(math.sqrt(6.28),10)*math.pow(np.linalg.det(cinv),0.5)
+    covariance=np.cov(x)
+    cinv=inv(covariance)
+    m=w*np.exp(np.multiply(-0.5,np.dot(np.dot(deltat,cinv),delta)))
+    d=math.sqrt(6.28*np.linalg.det(cinv))
     return m/d
 
 # Import data from file
 # Here I'm only importing 1 image
 # This has to be repested for all 60 images and each image sould be stacked on top of each other
 # Expected dimension 60x565x160
-im=cv2.imread("001_L_1.png")
-x=np.asarray(im)
+TrainingReal = np.load("TrainingReal.npy")
+TrainingSpoof = np.load("TrainingSpoof.npy")
+
 
 # Initilaize 2 means, sigma, and weights
-mu1 = np.ones((10,), dtype=np.int)
-sigma1 = np.ones((10,), dtype=np.int)
+mu1 = TrainingReal[1]
+sigma1 = TrainingReal[1]
 w1 = 0.5
 
-mu2 = np.ones((10,), dtype=np.int)
-sigma2 = np.ones((10,), dtype=np.int)
+mu2 = TrainingSpoof[1]
+sigma2 = TrainingSpoof[1]
 w2 = 0.5
 
 # Assign constants like no. of samples, no. of features.
@@ -34,9 +36,9 @@ k=2
 
 for num in range(1,1000):
     # Compute initial Expectation with the given values, and assign each data point to either of the class
-    for i in range(0,59):
-        cluster1[i] = find_val(w1,mu1,sigma1,x[i])
-        cluster2[i] = find_val(w2,mu2,sigma2,x[i])
+    for i in range(0,119):
+        cluster1[i] = find_val(w1,mu1,sigma1,TrainingReal[i])
+        cluster2[i] = find_val(w2,mu2,sigma2,TrainingReal[i])
         c1[i]=cluster1[i]/(cluster1[i]+cluster2[i])
         c2[i]=1-c1[i]
 
