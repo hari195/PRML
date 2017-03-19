79acdc5b0990c5e2e80ce3e78922dccf7c7c8480
Harikrishnan R <harikrishnanr195@gmail.com>
2017-03-19 13:57:13 +0530

    Made a strucutre

---
+++ gmm.py
@@ -0,0 +1,7 @@
+# Initilaize 2 means, sigma, and weights
+# Import data from file
+# Assign constants like no. of samples, no. of features.
+# Compute initial Expectation with the given values, and assign each data point to either of the class
+# Recompute 6 paramters.
+# Check for convergence
+# Repeat until convergence
--- image_read.py
+++ image_read.py
@@ -1,3 +1,11 @@
 import cv2
-im=cv2.imread("/TrainingReal/001_L_1.png")
-print type(im)
+import numpy as np
+im=cv2.imread("001_L_1.png")
+im_bw= cv2.threshold(im, 200, 255, cv2.THRESH_BINARY)[1]
+cv2.imshow('im_bw', im_bw)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+x=np.asarray(im)
+print x[100,23,0]
+print x[100,23,1]
+print x[100,23,2]
